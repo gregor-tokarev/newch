152b9c0110b44f374243dd53dff82f845527c339
Egor Tokarev <zippper123915@gmail.com>
2021-01-01 02:18:40 +0300

    build(save): save all files

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,8 +34,16 @@ export default {
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
     // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios'
+    '@nuxtjs/svg',
+    '@nuxtjs/axios',
+    '@nuxtjs/firebase'
   ],
+
+  firebase: {
+    config: {
+      projectId: 'newch-1f863'
+    }
+  },
 
   // Axios module configuration (https://go.nuxtjs.dev/config-axios)
   axios: {},
--- stylelint.config.js
+++ stylelint.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   extends: [
-    'stylelint-config-standard'
+    'stylelint-config-standard',
+    'stylelint-config-recess-order'
   ],
   // add your custom config here
   // https://stylelint.io/user-guide/configuration
